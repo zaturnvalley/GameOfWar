73acb15477ca724d1fc5880ecf0af4e9a89f2331
zaturnvalley <zacharycmason@gmail.com>
2016-08-02 11:00:46 -0700

    made js dry, declare winner function, fixed shuffle function

--- js/code.js
+++ js/code.js
@@ -1,110 +1,109 @@
 
-//all cards
-var fullDeck = [3, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15];
-
+//global variables
+var fullDeck = [];
 var player1 = [];
 var player2 = [];
+var bounty = [];
+var p1Play;
+var p2Play;
+var WAR_BOUNTY = 3;
 
-//shuffle the cards
+//shuffles the cards
 function shuffle(){
+  player1 = [];
+  player2 = [];
+  fullDeck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15];
   fullDeck.sort(function() { return 0.5 - Math.random() });
 }
 //deals half to player 1 half to player 2 after shuffle
 function deal(){
-  fullDeck = [3, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15];
   player1 = fullDeck.splice(0,27);
   player2 = fullDeck;
-  fullDeck = [];
   console.log('dealing p1 ',player1)
-  console.log('dealing p2', player2)
+  console.log('dealing p2 ', player2)
 }
 
-//invokes deal and shuffle
+//invokes deal and shuffle on button click
 $('#shuffle').click(function(){
+  console.log('clicked');
   shuffle();
   deal();
   $('#shuffle').html('Re-Shuffle');
+  $('#player1').bind('click', clicker);
 });
-
-$('#reset').click(function(){
-  fullDeck = [3, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15];
-  player1 = [];
-  player2 = [];
-});
-
-function remClass(player, i){
-  $('"' + '#' + '"' + player).removeClass('"' + '.' + i + '"');
-}
-function adClass(player, i){
-  $('"' + '#' + '"' + player).addClass('"' + '.' + i + '"');
-}
-//Turn function
-$('#player1').click(function(){
-  if(fullDeck[0]!==undefined){
-    alert('Please Shuffle the Deck');
-    return;
-  }
+//turn function, checks winning scenario, calls round
+var clicker = function(){
   //Winner scenario
   if(player1.length === 54){
-  console.log('Player 1 Wins!');
+    declareWinner("Player 1");
+    return;
   }
   if(player2.length === 54){
-  console.log('Player 2 Wins!');
+    declareWinner("Player 2");
+    return;
   }
-  console.log("player1", player1[0]);
-  console.log("player2", player2[0]);
-
-  if(player1[0] > player2[0]){
-    player1.push(player2.shift());
-    player1.push(player1.shift());
-    $('p').val('Player 1 Takes');
+  round();
+};
+//activates each round
+function round(){
+  if(player1.length === 0){
+    declareWinner("Player 2");
+    return;
+  }
+  if(player2.length === 0){
+    declareWinner("Player 1");
+    return;
+  }
+  p1Play = player1.shift();
+  p2Play = player2.shift();
+    bounty.push(p1Play);
+    bounty.push(p2Play);
+  displayCard(p1Play, p2Play);
+  if(p1Play > p2Play){
+    player1.concat(bounty);
+    bounty = [];
+    $('#infoBox').html('Player 1 Takes');
     console.log('p1');
-  } else if (player2[0] > player1[0]){
-    player2.push(player2.shift());
-    player2.push(player1.shift());
-    $('p').val('Player 2 Takes');
+  } else if (p1Play < p2Play){
+    player2.concat(bounty);
+    bounty = [];
+    $('#infoBox').html('Player 2 Takes');
     console.log('p2');
   } else {
-    $('p').val('War has begun');
-    war(3);
-  }
-});
-
-//if cards are tied, war is invoked
-function war(indexNum){
-  console.log('warring');
-  if(player1[indexNum] > player2[indexNum]){
-    var spliced2 = player2.splice(0,4);
-    player1.push(spliced2[indexNum - 3], spliced2[indexNum - 2], spliced2[indexNum - 1], spliced2[indexNum]);
-    $('p').val('Player 1 Wins War Round');
-  } else if(player2[indexNum] > player1[indexNum]){
-    var spliced1 = player1.splice(0,4);
-    $('p').val('Player 2 Wins War Round');
-    player2.push(spliced1[indexNum - 3], spliced1[indexNum - 2], spliced1[indexNum - 1], spliced1[indexNum]);
-  } else if (player2[indexNum]===player1[indexNum]){
-    console.log('rewar');
-    $('p').val('Tie! Re-war!');
-    war(indexNum + 4);
+    $('#infoBox').html('War has begun');
+    war();
   }
 }
-
-
-
-//rand divies all cards: 26 to p1, 26 to p2
-//first card in new array flip p1 & p2
-//compare: if p1 higher, add to p1 bottom, if p2 higher, add to p2 bottom
-//if same = war, p1 p2 flip 3 upsidedown, both flip 4th card up, compare
-//winner take all. if same, repeat another war
-//game ends when p1 or p2 has all the cards
-
-//bonus* if mad, press mad button. start 52 card pick up function
-//player must click all 52 scattered cards in 52 seconds. game resets
+//if cards are tied, war is invoked, calls round
+function war(){
+  console.log('warring');
+  if (player1.length < WAR_BOUNTY){
+    declareWinner("Player 2");
+    return;
+  }
+  if (player2.length < WAR_BOUNTY){
+    declareWinner("Player 1");
+    return;
+  }
+  for (var i = 0; i < WAR_BOUNTY; i++){
+    bounty.push(player1.shift());
+    bounty.push(player2.shift());
+  }
+  round();
+}
+//Declare winner
+function declareWinner(playerThatWon){
+  $('#player1').unbind('click', clicker);
+  console.log(playerThatWon + " is the Winner!");
+  $('#infoBox').html(playerThatWon + " is the Winner!");
+}
+//switches classes to display card
+function displayCard(cardNum1, cardNum2){
+  $('#p1InPlayCard').removeClass();
+  $('#p1InPlayCard').addClass("." + cardNum1);
+  $('#p2InPlayCard').removeClass();
+  $('#p2InPlayCard').addClass("." + cardNum2);
+}
